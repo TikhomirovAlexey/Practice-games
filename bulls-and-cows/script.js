0e416f30d6277f8d71c2e9efec002a3f3d394f88
Alexey <tihomir.94@mail.ru>
2025-01-29 14:46:48 +0300

    adedd html/css

--- bulls-and-cows/script.js
+++ bulls-and-cows/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const buttonsBox = document.querySelector('.buttonsBox');
+const inputEl = document.querySelector('.input');
+const textForUserInfo = document.querySelector('.text-for-user-info');
+const userAnswerBox = document.querySelector('.user-answer-box');
+const bullsCount = document.querySelector('.bulls-count');
+const cowsCount = document.querySelector('.cows-count');
